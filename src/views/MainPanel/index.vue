ce9ed433998b1358ac47d5c1f8b5d2df44855802
ping.wang <ping.wang@bettertree.cn>
2020-07-20 10:16:34 +0800

    公告内容

--- src/views/MainPanel/index.vue
+++ src/views/MainPanel/index.vue
@@ -22,6 +22,12 @@
         </el-form-item>
       </el-form>
     </div>
+    <el-alert
+      title="消息提示的文案"
+      type="info"
+      description="文字说明文字说明文字说明文字说明文字说明文字说明"
+      show-icon>
+    </el-alert>
     <main style="height: 760px">
       <ul>
         <li
